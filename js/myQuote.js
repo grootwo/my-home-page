0d8808fc4ac5f59ab0fc7c29ccd31e593018b299
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-23 10:06:37 +0900

    Show quote when submit

--- js/myQuote.js
+++ js/myQuote.js
@@ -8,51 +8,52 @@ const infoInput = quoteForm.querySelector("input:last-child");
 const QUOTES_KEY = "myQuotes";
 
 let myQuotes = [
-    {
-        quote: "일찍, 많이 실패하자!",
-        info: "23.01.27.금",
-    },
-    {
-        quote: "Don't wait for somebody to ask you.",
-        info: "Reese Witherspoon",
-    },
-    {
-        quote: "모르고 넘어가는 게 더 부끄러운 일이다.",
-        info: "그러니 질문하자",
-    },
+  {
+    quote: "일찍, 많이 실패하자!",
+    info: "23.01.27.금",
+  },
+  {
+    quote: "Don't wait for somebody to ask you.",
+    info: "Reese Witherspoon",
+  },
+  {
+    quote: "모르고 넘어가는 게 더 부끄러운 일이다.",
+    info: "그러니 질문하자",
+  },
 ];
 
 function quoteSubmit(event) {
-    event.preventDefault();
-    const newQuote = {
-        quote: quoteInput.value,
-        info: infoInput.value,
-    };
-    myQuotes.push(newQuote);
-    quoteInput.value = "";
-    infoInput.value = "";
-    saveQuotes();
+  event.preventDefault();
+  const newQuote = {
+    quote: quoteInput.value,
+    info: infoInput.value,
+  };
+  myQuotes.push(newQuote);
+  quoteInput.value = "";
+  infoInput.value = "";
+  saveQuotes();
+  paintQuote();
 }
 function saveQuotes() {
-    localStorage.setItem(QUOTES_KEY, JSON.stringify(myQuotes));
+  localStorage.setItem(QUOTES_KEY, JSON.stringify(myQuotes));
 }
 function loadQuotes() {
-    myQuotes = JSON.parse(localStorage.getItem(QUOTES_KEY));
+  myQuotes = JSON.parse(localStorage.getItem(QUOTES_KEY));
 }
 function paintQuote() {
-    loadQuotes();
-    const chosenQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
-    quote.innerText = chosenQuote.quote;
-    info.innerText = chosenQuote.info;
+  loadQuotes();
+  const chosenQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
+  quote.innerText = chosenQuote.quote;
+  info.innerText = chosenQuote.info;
 }
 
 quoteForm.addEventListener("submit", quoteSubmit);
 
 loadQuotes();
 if (myQuotes === null) {
-    myQuotes = [];
-    saveQuotes();
-    paintQuote();
+  myQuotes = [];
+  saveQuotes();
+  paintQuote();
 } else {
-    paintQuote();
+  paintQuote();
 }
